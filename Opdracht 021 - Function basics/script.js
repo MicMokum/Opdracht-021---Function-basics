ad95865b63458416aa74a7536f93df303beb9207
MicMokum <80250147+MicMokum@users.noreply.github.com>
2021-03-26 11:59:31 +0100

    Add files via upload

---
+++ Opdracht 021 - Function basics/script.js
@@ -0,0 +1,23 @@
+const makePizza = function() {
+    console.log("Maak deeg");
+    console.log("Maak saus");
+    console.log("Rol deeg uit");
+    console.log("Doe saus op deeg");
+    console.log("Kaas");
+    console.log("Oven");
+};
+
+const makeSushi = function() {
+    console.log("Iets met rijst");
+    console.log("Iets met zeewier");
+    console.log("Pak emmer");
+    console.log("Iets met vis");
+    console.log("Kots in emmer");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
